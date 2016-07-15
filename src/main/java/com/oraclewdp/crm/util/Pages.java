64928c6f98ce5c878af4132c51e665045eb5a370
HubuClearlove <1036184285@qq.com>
2016-07-15 09:44:49 +0800

    没有代码的更改

--- src/main/java/com/oraclewdp/crm/util/Pages.java
+++ src/main/java/com/oraclewdp/crm/util/Pages.java
@@ -8,12 +8,12 @@ import java.util.List;
  * @param <T>
  */
 public interface Pages<T> {
-    public void toPage(int page);
+    public void toPage(int page);  //
 
-    public int getAllCount();
-    public List<T> getItems();
-    public int getPageCount() ;
-    public int getCurrentPage();
+    public int getAllCount(); //总页数
+    public List<T> getItems(); 
+    public int getPageCount() ;  //总条数
+    public int getCurrentPage(); //当前页面
 
-    public int getPageSize();
+    public int getPageSize(); // 当前页面数
 }
