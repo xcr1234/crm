66978311d6cb46ecfb221e0041248c11e57b2775
HubuClearlove <1036184285@qq.com>
2016-07-16 03:41:53 +0800

    基于数据表的改动，实体类也做出相应的更改，并重写tostring方法。

--- src/main/java/com/oraclewdp/crm/entity/Customer.java
+++ src/main/java/com/oraclewdp/crm/entity/Customer.java
@@ -26,125 +26,143 @@ public class Customer {
 
     private String createdate;
 
-    private String creator;
+    @Column(value = "creator_id",foreign = "user",target = "id")
+    private User creator;
+   
+    @Column(value = "sales_id",foreign = "user",target = "id")
+    private User sales;
 
-    private String sales;
+    @Column(value = "provence_id",foreign = "base_area",target = "id")
+    private BaseArea provence;
 
-    private String provence;
+    @Column(value = "city_id",foreign = "base_area",target = "id")
+    private BaseArea city;
 
-    private String city;
+    @Column(value = "county_id",foreign = "base_area",target = "id")
+    private BaseArea county;
 
-    private String county;
+	public Integer getId() {
+		return id;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public String getCode() {
+		return code;
+	}
 
-    public String getCode() {
-        return code;
-    }
+	public void setCode(String code) {
+		this.code = code;
+	}
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+	public DicAll getType() {
+		return type;
+	}
 
-    public DicAll getType() {
-        return type;
-    }
+	public void setType(DicAll type) {
+		this.type = type;
+	}
 
-    public void setType(DicAll type) {
-        this.type = type;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    public String getPhone() {
-        return phone;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getAdress() {
+		return adress;
+	}
 
-    public String getAdress() {
-        return adress;
-    }
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
 
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
+	public String getQq() {
+		return qq;
+	}
 
-    public String getQq() {
-        return qq;
-    }
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
 
-    public void setQq(String qq) {
-        this.qq = qq;
-    }
+	public String getCreatedate() {
+		return createdate;
+	}
 
-    public String getCreatedate() {
-        return createdate;
-    }
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
 
-    public void setCreatedate(String createdate) {
-        this.createdate = createdate;
-    }
+	public User getCreatorId() {
+		return creator;
+	}
 
-    public String getCreator() {
-        return creator;
-    }
+	public void setCreatorId(User creator) {
+		this.creator = creator;
+	}
 
-    public void setCreator(String creator) {
-        this.creator = creator;
-    }
+	public User getSales() {
+		return sales;
+	}
 
-    public String getSales() {
-        return sales;
-    }
+	public void setSales(User sales) {
+		this.sales = sales;
+	}
 
-    public void setSales(String sales) {
-        this.sales = sales;
-    }
+	public BaseArea getProvence() {
+		return provence;
+	}
 
-    public String getProvence() {
-        return provence;
-    }
+	public void setProvence(BaseArea provence) {
+		this.provence = provence;
+	}
 
-    public void setProvence(String provence) {
-        this.provence = provence;
-    }
+	public BaseArea getCity() {
+		return city;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public void setCity(BaseArea city) {
+		this.city = city;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public BaseArea getCounty() {
+		return county;
+	}
 
-    public String getCounty() {
-        return county;
-    }
+	public void setCounty(BaseArea county) {
+		this.county = county;
+	}
 
-    public void setCounty(String county) {
-        this.county = county;
-    }
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", code=" + code + ", type=" + type
+				+ ", name=" + name + ", phone=" + phone + ", email=" + email
+				+ ", adress=" + adress + ", qq=" + qq + ", createdate="
+				+ createdate + ", creator=" + creator + ", sales=" + sales
+				+ ", provence=" + provence + ", city=" + city + ", county="
+				+ county + "]";
+	}
+
+	
+    
 }
