0dcf56d96ce0f64db7db8b50c246c84d3d765b47
xcr <530551426@qq.com>
2016-07-16 21:54:41 +0800

    修复一个问题

--- src/main/webapp/js/city.js
+++ src/main/webapp/js/city.js
@@ -423,6 +423,27 @@ dsy.add("0_33",["台北","高雄","台中","花莲","基隆","嘉义","金门","
 dsy.add("0",["北京市","天津市","上海市","重庆市","河北省","山西省","内蒙古","辽宁省","吉林省","黑龙江省","江苏省","浙江省","安徽省","福建省","江西省","山东省","河南省","湖北省","湖南省","广东省","广西","海南省","四川省","贵州省","云南省","西藏","陕西省","甘肃省","青海省","宁夏","新疆","香港","澳门","台湾省"]);
 
 
+function change(v){
+    var str="0";
+    for(i=0;i<v;i++){
+        str+=("_"+(document.getElementById(s[i]).selectedIndex-1));
+    };
+    var ss=document.getElementById(s[v]);
+    with(ss){
+        length = 0;
+        options[0]=new Option(opt0[v],opt0[v]);
+        if(v && document.getElementById(s[v-1]).selectedIndex>0 || !v){
+            if(dsy.Exists(str)){
+                ar = dsy.Items[str];
+                for(i=0;i<ar.length;i++){
+                    options[length]=new Option(ar[i],ar[i]);
+                }//end for
+                if(v){ options[0].selected = true; }
+            }
+        }//end if v 
+        if(++v<s.length){change(v);}
+    }//End with
+}
 
 function _init_area(){  //初始化函数
         for(i=0;i<s.length-1;i++){
