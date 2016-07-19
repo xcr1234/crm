4981181a9414a2bf321b2888b3a6bbedff1cb34e
xcr <530551426@qq.com>
2016-07-19 23:08:23 +0800

    一个有用的Listener

---
+++ src/main/java/com/oraclewdp/crm/util/DaoImplListener.java
@@ -0,0 +1,35 @@
+package com.oraclewdp.crm.util;
+
+import com.oraclewdp.crm.dao.impl.DaoImpl;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebListener
+public class DaoImplListener implements ServletRequestListener {
+    @Override
+    public void requestDestroyed(ServletRequestEvent event) {
+        JdbcUtil jdbcUtil = JdbcUtil.getInstance();
+        try {
+            jdbcUtil.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent event) {
+        JdbcUtil jdbcUtil = JdbcUtil.getInstance();
+        Connection connection = jdbcUtil.getConnection();
+        DaoImpl dao = new DaoImpl(connection);
+
+        event.getServletRequest().setAttribute("dao",dao);
+
+
+
+    }
+}
